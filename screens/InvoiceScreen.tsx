b34b19bfd72997f822b47332a291e1c0175457f1
VivianaS14 <vasorianp@gmail.com>
2024-01-29 13:28:43 -0500

    App screens

---
+++ screens/InvoiceScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const InvoiceScreen = () => {
+  return (
+    <View>
+      <Text>InvoiceScreen</Text>
+    </View>
+  );
+};
+
+export default InvoiceScreen;
---
+++ screens/JobsScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const JobsScreen = () => {
+  return (
+    <View>
+      <Text>JobsScreen</Text>
+    </View>
+  );
+};
+
+export default JobsScreen;
